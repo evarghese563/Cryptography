6eb6019437702c377feff8162b3a87c21ade0728
evarghese563 <54088828+evarghese563@users.noreply.github.com>
2023-10-22 12:39:05 -0400

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,14 @@
+import math
+
+print("Enter val1")
+val1 = int(input())
+
+print("Enter val2")
+val2 = int(input())
+
+gcd = math.gcd(val1,val2)
+
+if gcd == 1:
+    print("Coprime: ", gcd)
+else:
+    print("Not Coprime: ", gcd)
